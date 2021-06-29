e9790dd22b849e2939796242a50b9fdd918c3926
unknown <nirajdighe005@gmail.com>
2021-06-29 19:22:44 +0530

    (+) Created a new spring boot project for straddle strategy.

---
+++ src/main/java/com/straddler/appStart/StraddlerApplication.java
@@ -0,0 +1,13 @@
+package com.straddler.appStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StraddlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StraddlerApplication.class, args);
+	}
+
+}
